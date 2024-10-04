d0c22e6d04b9fa487ace98678e97814f38af7897
nstntahckdcb <55213500+nstntahckdcb@users.noreply.github.com>
2024-10-03 21:27:12 -0500

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,138 +1,90 @@
-#include "LinkedList.h" 
+#include "LinkedList.h"
 #include <iostream>
-using namespace std;
 
-//default constructor
-List::List(){
-	int length = 0;
-	head = NULL;
-	current = NULL;
+// Constructor
+LinkedList::LinkedList() : head(nullptr) {}
+
+// Copy constructor
+LinkedList::LinkedList(const LinkedList& otherList) {
+    head = nullptr;
+    Node* current = otherList.head;
+    while (current != nullptr) {
+        insert(new Ant(*current->data));  // Make a deep copy of each ant
+        current = current->next;
+    }
 }
 
-//copy constructor
-List::List(const List& otherList){
-	if(otherList.head == NULL){
-		head = NULL;
-		current = NULL;
-		int length = 0;
-	}else{
-		head = new Node(otherList.head->data);
-		current = head;
-		length++;
-		Node* temp = otherList.head;
-		while(temp -> next != NULL){
-			current -> next = new Node(temp->next->data);
-			current = current-> next;
-			temp = temp->next;
-			length++;
-		}
-	}
-
+// Destructor
+LinkedList::~LinkedList() {
+    Node* current = head;
+    while (current != nullptr) {
+        Node* next = current->next;
+        delete current->data;  // Free each Ant
+        delete current;
+        current = next;
+    }
 }
 
-//destructor
-List::~List(){
-
-	Node* temp = head;
-	Node *next = NULL;
-	while(temp != NULL){
-		next = temp->next;
-		delete temp;
-		temp = next;
-	}
-
+// Insert an ant at the head of the list
+void LinkedList::insert(Ant* ant) {
+    Node* newNode = new Node(ant);
+    newNode->next = head;
+    head = newNode;
 }
 
-
-
-void List::insert(Ant* ant){
-     if(head == NULL){
-	head = new Node(ant);
-     }
-     else if(head != NULL){
-
-	Node* temp = this -> head;
-	while(temp->next != NULL){
-		temp = temp->next;
-	}
-	temp->next = new Node(ant);
-     }
-     length++;
+// Get the first ant in the list
+Ant* LinkedList::get() {
+    if (head != nullptr) {
+        return head->data;
+    }
+    return nullptr;
 }
 
-
-//get next values
-Ant* List::get(){
-	if(current == NULL){
-		current = head;
-	}
-	Ant* val = current -> data;
-	if(current->next != NULL){
-		current = current->next;
-	}
-	return val;
+// Reset the head of the list (if needed)
+void LinkedList::reset() {
+    head = nullptr;
 }
 
-void List::reset(){
-	current = NULL;
+// Print the list of ants
+void LinkedList::print() {
+    for (Node* temp = head; temp != nullptr; temp = temp->next) {
+        std::cout << "Ant ID: " << temp->data->id << " at (" << temp->data->x << "," << temp->data->y << ")\n";
+    }
 }
 
-//print
-void List::print(){
+// Remove an ant by pointer (returns true if successful)
+bool LinkedList::remove(Ant* ant) {
+    Node* current = head;
+    Node* prev = nullptr;
 
-	for(Node* temp = this-> head; temp != NULL; temp = temp->next){
-		cout << temp -> data -> getID() << endl;
-	}
-
+    while (current != nullptr) {
+        if (current->data == ant) {
+            if (prev == nullptr) {
+                head = current->next;  // Remove the head
+            } else {
+                prev->next = current->next;  // Remove from the middle or end
+            }
+            delete current->data;
+            delete current;
+            return true;
+        }
+        prev = current;
+        current = current->next;
+    }
+    return false;
 }
 
-bool List::remove(Ant *ant)
-{
-	Node *temp = this->head;
-	Node *prev = NULL;
-
-	while (temp)
-	{
-		if (temp->data->getID() == ant->getID())
-		{
-			if (prev)
-				prev->next = temp->next;
-			else
-				this->head = temp->next;
-
-			delete temp->data;
-			delete temp;
-			temp = NULL;
-			return true;
-		}
-		else
-		{
-			prev = temp;
-			temp = temp->next;
-		}
-	}
-
-	return false;
+// Find an ant by ID
+Ant* LinkedList::find(int id) {
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data->id == id) {
+            return current->data;
+        }
+        current = current->next;
+    }
+    return nullptr;
 }
-
-
-Ant* List::find(int val){
-	Node* temp = this->head;
-	while(temp){
-		if(temp->data->getID() == val){
-			return temp->data;
-		}else{
-			temp = temp->next;
-		}
-	}
-	return NULL;
-
-}
-
-void List::operator << (Ant* other){
-	this->insert(other);
-}
-
 
 
 

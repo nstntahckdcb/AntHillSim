27fded0a0a38c3c80304cd34423bbfea3a0223b9
nstntahckdcb <55213500+nstntahckdcb@users.noreply.github.com>
2024-10-03 21:19:07 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,37 +1,24 @@
+// LinkedList.h (Minimalist template for handling ants)
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-#include <cstddef>
-#include "Ant.h"
+template <typename T>
+class LinkedList {
+public:
+    struct Node {
+        T data;
+        Node *next;
+        Node(T d) : data(d), next(nullptr) {}
+    };
 
-class Node{
-    public:
-      Node* next;
-      Ant* data;
-      Node(Ant* ant){
-	  this -> data = ant;
-          this -> next = NULL;
-      }
+    Node *head;
+
+    LinkedList() : head(nullptr) {}
+    void insert(T data) {
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
 };
 
-class List{
-
-  private:
-	Node* head;
-	Node* current;
-
-  public:
-	List();
-	List(const List&);
-	~List();
-	int length;
-	void insert(Ant* ant);
-	void print();
-	Ant* get();
-	void reset();
-	bool remove(Ant* ant);
-	Ant* find(int val);
-	void operator << (Ant* other);
-};
-
-#endif
+#endif // LINKEDLIST_H

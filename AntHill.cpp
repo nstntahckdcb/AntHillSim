ad0d5463f93830ee9fa2c7342e6957a58b6fdae5
nstntahckdcb <55213500+nstntahckdcb@users.noreply.github.com>
2024-10-03 21:32:02 -0500

    Update AntHill.cpp

--- AntHill.cpp
+++ AntHill.cpp
@@ -1,30 +1,29 @@
-// AntHill.cpp
 #include "AntHill.h"
 #include <iostream>
 
 AntHill::AntHill(int numAnts) {
     for (int i = 0; i < numAnts; i++) {
-        ants.insert(Ant(i));
+        ants.insert(new Ant(i));  // Pass a pointer to a new Ant
     }
 }
 
 void AntHill::simulateStep() {
-    LinkedList<Ant>::Node *current = ants.head;
+    LinkedList::Node *current = ants.head;  // Remove template syntax
     while (current != nullptr) {
-        current->data.move(); // Each ant moves
+        current->data->move();  // Each ant moves
         // Simulate fight randomly between two ants
         if (rand() % 5 == 0 && current->next != nullptr) {
-            current->data.fight(current->next->data);
+            current->data->fight(*(current->next->data));
         }
         current = current->next;
     }
 }
 
 void AntHill::showAnts() {
-    LinkedList<Ant>::Node *current = ants.head;
+    LinkedList::Node *current = ants.head;  // Remove template syntax
     while (current != nullptr) {
-        Ant &ant = current->data;
-        std::cout << "Ant #" << ant.id << " at (" << ant.x << "," << ant.y << ") Health: " << ant.health << "\n";
+        Ant *ant = current->data;
+        std::cout << "Ant #" << ant->id << " at (" << ant->x << "," << ant->y << ") Health: " << ant->health << "\n";
         current = current->next;
     }
 }

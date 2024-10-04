7cc3b65fe6c3b6c2207794e02ce0ad77158025a9
nstntahckdcb <55213500+nstntahckdcb@users.noreply.github.com>
2024-10-03 21:17:39 -0500

    Update AntHill.h

--- AntHill.h
+++ AntHill.h
@@ -1,38 +1,17 @@
-
+// AntHill.h
 #ifndef ANTHILL_H
 #define ANTHILL_H
 
-#include "Ant.h"
 #include "LinkedList.h"
-#include <iostream>
-#include <fstream>
+#include "Ant.h"
 
-class AntHill{
-	private:
-	   int current_ants;
-	   int id;
-	   int food;
-	   int turnNum;
-	   int success;
-	   int loss;
-   	   std::ofstream out;//("anthill.log");
-
-	public:
-	   List* ants;
-	   AntHill();
-	   ~AntHill();
-	   void turn();
-	   void addFood();
-	   void check(Ant* ant);
-	   void addAnt();
-	   bool removeAnt(Ant* ant);
-	   Ant* getAnt(int id_num);
-	   void move();
-	   bool changeMax(int max);
-	   bool attack();
-	   void printHillInfo();
-	   int getCurrentNumAnts(){return current_ants;};
-//	   Ant* fight(Ant* challenger);
+class AntHill {
+private:
+    LinkedList<Ant> ants;
+public:
+    AntHill(int numAnts);
+    void simulateStep(); // Simulate one step of the simulation
+    void showAnts(); // Display ants' status
 };
 
-#endif
+#endif // ANTHILL_H
